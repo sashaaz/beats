360eca838d31584836f3991dcc08c0afbf5561a3
sashaaz <faerieixi@gmail.com>
2018-03-13 13:27:56 -0500

    final phases/graphs
    looks good right?? :)

--- phases/phases.m
+++ phases/phases.m
@@ -1,41 +1,43 @@
 t = -100:0.1:100;
 
-w = [0.8 1 1.2];
+w = [0.9 1 1.1];
 
 
 % Want phi_1 =/= phi_2 =/= phi_3, but 2phi_2 = phi_1 + phi_3
 %dE = [0.1 0.6 1.1; 0.3 0.8 1.3; 0.2 0.7 1.2]*pi; % final matrix for 2phi_2 -phi_1 - phi_3 const.
 
-%dE = [0 -1/2 -1; 0 1 2; 0 -1/2 -1]*pi; % final matrix for phi_1 + phi_2 + phi_3 const.
+dE = [0 -1/2 -1; 0 1 2; 0 -1/2 -1]*pi; % final matrix for phi_1 + phi_2 + phi_3 const.
 
-dE = [0 1/2 1; 0 0 0; 0 -1/2 -1]*pi; % matrix for phi_1 - phi_3 not constant, adding linear time delay.
-% the values that are here now are not exactly right, but they are close to
-% right. Give nearly nondistorted time delay.
-% figure; 
-% hold on;
+w0 = 1;
+Omega = 0.1;
 
-E = zeros(3, size(t, 2));
+phi0 = [0 0.5 1]*pi;
+dphi = 0; % which == Phi
+delta = 0;
+
+w = [w0 - Omega, w0, w0 + Omega];
+
+
+E = zeros(length(phi0), size(t, 2));
 figure
 hold on
-for i = 1:length(w)
-    for j=1:length(w)
-        E(j, :) = E(j, :) + cos(w(i)*t + dE(i, j));
+for j=1:length(phi0)
+    phi_minus = phi0(j) + (w0 - Omega)*delta/Omega;
+    phi = phi0(j) + dphi + w0 * delta / Omega;
+    phi_plus = phi0(j) + (w0 + Omega)*delta/Omega;
+
+    phi = [phi_minus, phi, phi_plus];
+    for i = 1:length(w)
+        E(j, :) = E(j, :) + cos(w(i)*t + phi(i));
     end
 end
-for j=1:length(w)
+legends = {};
+for j=1:length(phi0)
     plot(t, 1/3 * E(j, :))
+    legends{j} = ['$\phi_0 =$ ',  num2str(phi0(j)/pi), '$\pi$'];
 end
-legend({'$\frac{1}{2} (\phi_1 - \phi_3) = 0$', '$\frac{1}{2} (\phi_1 - \phi_3) = ?$',...
-'$\frac{1}{2} (\phi_1 - \phi_3) = \pi?$'}, 'Interpreter','latex');
+
+legend(legends, 'Interpreter', 'latex');
 set(gca, 'fontsize', 26)
 xlabel('Time (t)');
 ylabel('Amplitude');
-
-% Peter code not used
-% for dphi = dphi
-%     E = zeros(size(t));
-%     for i = 1:length(w)
-%         E = E + cos(w(i)*t + dphi);
-%     end
-%     plot(E)
-% end
--- phases/phases_cond2.m
+++ phases/phases_cond2.m
@@ -6,23 +6,24 @@ w = [0.9 1 1.1];
 % Want phi_1 =/= phi_2 =/= phi_3, but 2phi_2 = phi_1 + phi_3
 %dE = [0.1 0.6 1.1; 0.3 0.8 1.3; 0.2 0.7 1.2]*pi; % final matrix for 2phi_2 -phi_1 - phi_3 const.
 
-%dE = [0 -1/2 -1; 0 1 2; 0 -1/2 -1]*pi; % final matrix for phi_1 + phi_2 + phi_3 const.
+dE = [0 -1/2 -1; 0 1 2; 0 -1/2 -1]*pi; % final matrix for phi_1 + phi_2 + phi_3 const.
+
 w0 = 1;
 Omega = 0.1;
 
 phi0 = 0;
-dphi = 0;
-delta = [0 0.5 1]*pi;
+dphi = [0 0.5 1]*pi; % which == Phi
+delta = [0 0 0];
 
 w = [w0 - Omega, w0, w0 + Omega];
 
 
-E = zeros(length(delta), size(t, 2));
+E = zeros(length(dphi), size(t, 2));
 figure
 hold on
-for j=1:length(delta)
+for j=1:length(dphi)
     phi_minus = phi0 + (w0 - Omega)*delta(j)/Omega;
-    phi = phi0 + dphi + w0 * delta(j) / Omega;
+    phi = phi0 + dphi(j) + w0 * delta(j)/ Omega;
     phi_plus = phi0 + (w0 + Omega)*delta(j)/Omega;
 
     phi = [phi_minus, phi, phi_plus];
@@ -31,13 +32,12 @@ for j=1:length(delta)
     end
 end
 legends = {};
-for j=1:length(delta)
+for j=1:length(dphi)
     plot(t, 1/3 * E(j, :))
-    legends{j} = ['$\Omega\Delta t =$ ',  num2str(delta(j)/pi), '$\pi$'];
+    legends{j} = ['$\Delta\phi =$ ',  num2str(dphi(j)/pi), '$\pi$'];
 end
 
 legend(legends, 'Interpreter', 'latex');
 set(gca, 'fontsize', 26)
 xlabel('Time (t)');
 ylabel('Amplitude');
-
